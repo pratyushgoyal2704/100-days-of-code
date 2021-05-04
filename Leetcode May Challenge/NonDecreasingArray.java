835cb285c03b2a1c006dad00834c0b4e3d2e69ac
Pratyush Goyal <42108127+pratyushgoyal2704@users.noreply.github.com>
2021-05-04 21:06:36 +0530

    Create NonDecreasingArray.java

---
+++ Leetcode May Challenge/NonDecreasingArray.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean checkPossibility(int[] nums) {
+        
+        int n = nums.length;
+        int cnt = 0;
+        
+        for(int i=1;i<n;i++){
+            if(nums[i]<nums[i-1]){
+                if(i==1 || nums[i-2]<=nums[i]){
+                    nums[i-1] = nums[i];
+                    cnt++;
+                }
+                else{
+                    nums[i] = nums[i-1];
+                    cnt++;
+                }
+            }
+        }
+        return cnt<=1;
+        
+    }
+}
+
+//good greedy 
