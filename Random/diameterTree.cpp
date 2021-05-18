e8d93aeafba9c99d777feb53cb9a75a8ed451d3e
Pratyush Goyal <42108127+pratyushgoyal2704@users.noreply.github.com>
2021-05-18 20:06:42 +0530

    Create diameterTree.cpp

---
+++ Random/diameterTree.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int height(TreeNode* root){
+        if(root==NULL) return 0;
+        
+        int left = 1+height(root->left);
+        int right = 1+height(root->right);
+        
+        return max(left,right);
+    }
+    
+
+    
+    int diameterOfBinaryTree(TreeNode* root) {
+        if(root==NULL) return 0;
+        
+        int root_dia = height(root->left)+height(root->right);
+        int left_dia = diameterOfBinaryTree(root->left);
+        int right_dia = diameterOfBinaryTree(root->right);
+        
+        
+        return max(root_dia,max(left_dia,right_dia));
+        
+    }
+};
+
+//Another approach using pair of height and dia, reduces calls
