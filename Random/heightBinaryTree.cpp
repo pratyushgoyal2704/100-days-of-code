ef4349892d7d496924772071638e84b2ff8c098c
Pratyush Goyal <42108127+pratyushgoyal2704@users.noreply.github.com>
2021-05-18 20:56:22 +0530

    Update heightBinaryTree.cpp

--- Random/heightBinaryTree.cpp
+++ Random/heightBinaryTree.cpp
@@ -3,9 +3,9 @@ public:
     
     int height(TreeNode* root){
         if(root==NULL) return 0;
-        int heightleft = 1+height(root->left);
-        int heightright = 1+height(root->right);
-        return max(heightleft,heightright);
+        int heightleft = (root->left);
+        int heightright = (root->right);
+        return 1+max(heightleft,heightright);
     }
     
     
